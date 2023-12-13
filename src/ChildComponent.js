5c25d19c5349a9af868c67562f237fd6cb540b12
A.Gurin <eoew@yandex.ru>
2023-12-13 14:01:16 +0300

    Стартовый коммит

---
+++ src/ChildComponent.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import "./styles/child-style.scss";
+
+export default function ChildComponent() {
+  return <div className="child-component">I'm ChildComponent!</div>;
+}
